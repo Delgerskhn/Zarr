5eeabbc4b7dc67648c2c291870ce6e8c596441b0
Дэлгэрсайхан Баянмөнх <delgersaikhan2001@gmail.com>
2020-11-12 11:19:30 +0800

    database migration configuration added

--- ZarNet/Startup.cs
+++ ZarNet/Startup.cs
@@ -27,11 +27,20 @@ namespace ZarNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnectionProd")));
+            } else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            }
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
             /*services.AddAuthentication().AddFacebook(facebookOptions =>
             {
                 facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
